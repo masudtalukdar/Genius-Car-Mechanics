0de3f02d37ebb1e06102b6955dbf161fc6cbece2
masudtalukdar <masudtalukdar.cn@gmail.com>
2021-10-27 22:20:10 +0800

    first commit

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+console.log(process.env);
+const firebaseConfig = {
+  apiKey: 'AIzaSyApkxfIPcCz7eQGXMKYNOTYATroEGZCHzI',
+  authDomain: 'genius-car-mechanics-d7286.firebaseapp.com',
+  projectId: 'genius-car-mechanics-d7286',
+  storageBucket: 'genius-car-mechanics-d7286.appspot.com',
+  messagingSenderId: '580463964333',
+  appId: '1:580463964333:web:622e8f1dae60fb11f85624',
+};
+
+export default firebaseConfig;
